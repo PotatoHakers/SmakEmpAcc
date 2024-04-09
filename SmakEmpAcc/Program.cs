03ca071a47c54514a75a48175fdf6d1d85fecee5
PotatoHakers <s.herza@mail.ru>
2024-04-09 20:49:01 +0300

    Program changes

--- SmakEmpAcc/Program.cs
+++ SmakEmpAcc/Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,10 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
     app.UseExceptionHandler("/Home/Error");
-    
 }
 
 //app.UseHttpsRedirection();
